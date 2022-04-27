a98ec20d7ca4ade11a5e5288dee2aaec7b9c6dc2
takashi <yagita.takashi@gmail.com>
2022-04-27 14:04:39 +0900

    first commit

--- tests/unit/test_flask_eks_workshop_stack.py
+++ tests/unit/test_flask_eks_workshop_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from flask_eks_workshop.flask_eks_workshop_stack import FlaskEksWorkshopStack
+from _stack.flask_eks_workshop_stack import FlaskEksWorkshopStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in flask_eks_workshop/flask_eks_workshop_stack.py
